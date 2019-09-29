ad7d9f7398b70b2871f715e799daee0763a8f28d
Bl4cksh4rk1990 <31000929+Bl4cksh4rk1990@users.noreply.github.com>
2019-09-29 19:44:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -93,7 +93,18 @@ if (!id || !state || state.ack) {
 				msgId: uuid.generateUUID().replace(/-/g, '').toUpperCase(),
 				mac: obj.native.mac,
 				deviceType: obj.native.deviceType,
-				operation: '0',
+				operation: '5'
+				};
+				request.post({
+					url: ApiURL + '/userCenter/deviceService/deviceControl',
+					form: formData,
+					json: true
+				}, function (err, httpResponse, body) {});
+				formData = {
+				accessToken: AccessToken,
+				msgId: uuid.generateUUID().replace(/-/g, '').toUpperCase(),
+				mac: obj.native.mac,
+				deviceType: obj.native.deviceType,
 				targetPosition: TempTargetPosition
 			};
 			}
